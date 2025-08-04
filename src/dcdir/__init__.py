44f97140cd2ffcca3d8e0e9b41423f304515a412
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-08-04 19:26:37 +0100

    import everything in __init__.py

--- src/dcdir/__init__.py
+++ src/dcdir/__init__.py
@@ -1,5 +1,7 @@
 from .dcdir import (
     Handler as Handler,
+    register_handler as register_handler,
     FileConfig as FileConfig,
     DataclassDirectory as DataclassDirectory,
+    make_dataclass_directory as make_dataclass_directory,
 )
