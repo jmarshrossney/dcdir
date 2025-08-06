7cac74ff488cd1b18f7fba13797b205f11da469b
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-08-06 11:26:58 +0100

    fix missing decorator issues and add decorator for Handler class

--- src/dirconf/missing.py
+++ src/dirconf/missing.py
@@ -3,34 +3,67 @@ from os import PathLike
 from pathlib import Path
 from typing import Callable, Any
 
+from .handler import Handler
+
 
 class MISSING:
     """A sentinal value that may be used in place of missing data."""
 
 
-def skip_read(
-    test: Callable[[Path], bool] = lambda path: not path.exists(),
-    sentinal: Any = MISSING,
+# TODO: consider adding logging
+
+
+def handle_missing_in_read(
+    test: Callable[[Path], bool] = lambda path: path.exists(),
 ):
     def decorator(reader):
         @functools.wraps(reader)
         def wrapper(self, path: str | PathLike):
-            return sentinal if test(Path(path)) else reader(path)
+            return reader(self, path) if test(Path(path)) else MISSING
 
         return wrapper
 
     return decorator
 
 
-def skip_write(
-    test: Callable[[Path, Any], bool] = lambda path, data: data is MISSING,
-    sentinal: Any = MISSING,
+def handle_missing_in_write(
+    test: Callable[[Path, Any, ...], bool] = (
+        lambda path, data, **_: data is not MISSING
+    ),
 ):
     def decorator(writer):
         @functools.wraps(writer)
         def wrapper(self, path: str | PathLike, data: Any, **kwargs):
-            return sentinal if test(Path(path), data) else writer(path)
+            test(Path(path), data, **kwargs) and writer(self, path, data, **kwargs)
 
         return wrapper
+
+    return decorator
+
+
+def handle_missing(
+    test_on_read: Callable[[Path], bool] = lambda path: Path(path).exists(),
+    test_on_write: Callable[[Path, Any, ...], bool] = (
+        lambda path, data, **_: data is not MISSING
+    ),
+):
+    def decorator(cls: Handler):
+        original_read = cls.read
+        original_write = cls.write
+
+        @functools.wraps(original_read)
+        def wrapped_read(self, path: str | PathLike) -> Any:
+            return original_read(self, path) if test_on_read(Path(path)) else MISSING
+
+        @functools.wraps(original_write)
+        def wrapped_write(self, path: str | PathLike, data: Any, **kwargs) -> None:
+            test_on_write(Path(path), data, **kwargs) and original_write(
+                self, path, data, **kwargs
+            )
+
+        cls.read = wrapped_read
+        cls.write = wrapped_write
+
+        return cls
 
     return decorator
